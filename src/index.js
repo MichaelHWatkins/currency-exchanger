7c6f9c43a0effea5010f9159ec1d715d43e09f5f
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-21 20:17:33 -0800

    add a fix to the webpack

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
-// import ConverterService from './converter-service.js';
+import ConverterService from './converter-service.js';
 
 $(document).ready(function() {
     $("#currency-form").submit(function(event) {
       event.preventDefault();
-      let currency = $('#currency').val();
-      let amount = $('#amount').val();
-      console.log(currency);
-      console.log(amount.type());
+      //let currency = $('#currency').val();
+      //let amount = $('#amount').val();
+      ConverterService.getCurrency()
+      .then(function(response) {
+        console.log(response)
+      })
     })
 });
