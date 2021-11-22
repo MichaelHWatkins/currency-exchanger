7ee2453d9b2f005449a76d4eb1d19759ccfc5c92
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-21 21:33:02 -0800

    complete the index.js file

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ $(document).ready(function() {
     $("#currency-form").submit(function(event) {
       event.preventDefault();
       let currency = $('#currency').val();
-      //let amount = $('#amount').val();
+      let amount = $('#amount').val();
       ConverterService.getCurrency()
       .then(function(response) {
         let rates = response.conversion_rates;
         let rateEntries = Object.entries(rates);
         rateEntries.forEach(function(entry){
           if(entry[0] === currency){
-            console.log(true)
+            $('#output').text(`Currency rounded to the nearest whole dollar amount: $${Math.round(amount/entry[1])}`)
+          }
+          else{
+            $("#output").text("I'm sorry, there isn't a currency with the abbreviation you specified.")
           }
         })
       })
