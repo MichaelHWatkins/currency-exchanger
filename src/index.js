41c728a80870620d0b5526171941442ccb45010e
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-21 19:43:55 -0800

    add a WIP in the index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
+// import ConverterService from './converter-service.js';
+
 $(document).ready(function() {
-  
+    $("#currency-form").submit(function(event) {
+      event.preventDefault();
+      let currency = $('#currency').val();
+      let amount = $('#amount').val();
+      console.log(currency);
+      console.log(amount.type());
+    })
 });
